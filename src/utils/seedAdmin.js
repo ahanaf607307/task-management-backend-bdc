240b9c7638438868a7bba862ae31f1e09511754f
ahanaf607307 <ahanaf607307@gmail.com>
2025-11-01 11:54:29 +0600

    fix admin issue

--- src/utils/seedAdmin.js
+++ src/utils/seedAdmin.js
@@ -6,21 +6,21 @@ const bcryptjs  = require('bcryptjs')
 const seedAdmin = async () => {
   try {
     const isSupperAdminExist = await User.findOne({
-      email: "admin1@gmail.com",
+      email: "admin@gmail.com",
     });
     if (isSupperAdminExist) {
-      console.log("Supper Admin Already Exist");
+      console.log(" Admin Already Exist");
       return;
     }
-    console.log("Trying to create supper admin");
+    console.log("Trying to create  admin");
     const hashedPassword = await bcryptjs.hash(
-     "admin1@g",
+     "1qazxcvb",
       10
     );
 
     const payload = {
       name: "Admin",
-      email: "admin1@gmail.com",
+      email: "admin@gmail.com",
       password: hashedPassword,
       role: "admin",
       isVerified: true,
